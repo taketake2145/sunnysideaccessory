45beca3e4336573ad6852d51988183a7feaddf42
Takenori Kawakami <simplesimplesdesign@gmail.com>
2021-04-20 07:42:35 +0900

    Add promote message

--- include/works.php
+++ include/works.php
@@ -3,6 +3,10 @@
     <p class="works__description">
       <span class="nowrap">どこかなつかしさを感じつつ、</span><span class="nowrap">新しい日常をふりほどいて歩いてく。</span><span class="nowrap">私は私でいい、の。</span>
     </p>
+    <div class="works__explain">
+      <h1 class="works__title">News</h1>
+      <p>ヨコハマハンドメイドマルシェ2021 に出店します。<br>日時：5/8(土)、9(日) 11:00〜18:00<br>会場：パシフィコ横浜<br>出店情報 &raquo; <a href="https://handmade-marche.jp/c/19356" target="_blank">sunnyside_accessory</a><br>詳細 &raquo; <a href="https://handmade-marche.jp/" target="_blank">ヨコハマハンドメイドマルシェ</a></p>
+    </div>
     <div class="works__explain js-works-explain">
       <h1 class="works__title">Works</h1>
       <p>イヤリングやブレスレットを一つひとつ丁寧にハンドメイドしています。<br>ご相談、ご依頼、ご注文やご購入は、Twitterのダイレクトメッセージやインスタのコメント、Minneからお気軽にご連絡くださいませ。<br>ご購入いただいた方々や同じハンドメイド作家さんたちからの心温まる声に励まされています。<span class="nowrap">いつもありがとうございます。</span></p>
