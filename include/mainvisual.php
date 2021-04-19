1a622ce3873ea4dc225d0d2961cfcafb6c3cd17d
Takenori Kawakami <simplesimplesdesign@gmail.com>
2021-04-20 07:39:30 +0900

    add video

--- include/mainvisual.php
+++ include/mainvisual.php
@@ -1,6 +1,16 @@
 <div class="mainvisual">
   <div class="mainvisual__inner">
     <img class="mainvisual__copy" src="<?php echo ASSETS_PATH; ?>/images/icomoon/copy.svg" alt="ノスタルジックに揺られて">
-    <iframe src="<?php echo ASSETS_PATH; ?>/video/index.html" frameborder="0" scrolling="no" style="width: 100%; height: 100%"></iframe>
+    
+    <div class="video">
+      <div class="video__frame">
+        <video autoplay muted loop playsinline>
+          <source src="/wp-content/themes/sunnysideaccessory/assets/video/mainvisual.webm" type="video/webm">
+          <source src="/wp-content/themes/sunnysideaccessory/assets/video/mainvisual.mp4" type="video/mp4">
+          <img src="">
+        </video>
+      </div>
+    </div>
+    
   </div>
 </div>
